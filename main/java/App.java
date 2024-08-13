6432bfd6732e02813958922d49cddafd374c3aa1
Daniil Shabalin <modowhen@gmail.com>
2024-08-13 10:54:25 +0300

    changes commit

---
+++ main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Одинаковы ли ссылки на объекты класса HelloWorld: " +(bean == bean2));
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Одинаковы ли ссылки на объекты класса Cat: " +(cat1 == cat2));
+    }
+}
